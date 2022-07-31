3a71bfcc293b9e973145e013ab74a94d7dd6426d
alishawadhwaniya <aw377541@gmail.com>
2022-07-31 17:24:34 +0530

    Inital Commit

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+import urllib.request
 import cv2
-import numpy as np
+from flask import Flask, jsonify, request
 import face_recognition
 
-elon_img = cv2.cvtColor(face_recognition.load_image_file('elon.png'), cv2.COLOR_BGR2RGB)
-elon_test = cv2.cvtColor(face_recognition.load_image_file('jackma.png'), cv2.COLOR_BGR2RGB)
+app = Flask(__name__)
 
-faceLoc = face_recognition.face_locations(elon_img)[0]
-encodeElon = face_recognition.face_encodings(elon_img)[0]
-cv2.rectangle(elon_img, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
 
-faceLocTest = face_recognition.face_locations(elon_test)[0]
-encodeElonTest = face_recognition.face_encodings(elon_test)[0]
-cv2.rectangle(elon_test, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
+@app.route('/')
+def welcome():
+    return 'Server is Running.The endpoint to verify face is /authenticate and pass links for both images as ' \
+           'image and testImage in params'
 
-results = face_recognition.compare_faces([encodeElon], encodeElonTest)
-print(results)
 
-cv2.imshow('Elon Musk', elon_img)
-cv2.imshow('Elon Test', elon_test)
-cv2.waitKey(0)
+@app.route('/authenticate', methods=['POST'])
+def check_face():
+    with app.app_context():
+        image1 = urllib.request.urlopen(request.args.get('image'))
+        image2 = urllib.request.urlopen(request.args.get('testImage'))
+        img = cv2.cvtColor(face_recognition.load_image_file(image1), cv2.COLOR_BGR2RGB)
+        img_test = cv2.cvtColor(face_recognition.load_image_file(image2), cv2.COLOR_BGR2RGB)
+        encodeElon = face_recognition.face_encodings(img)[0]
+        encodeElonTest = face_recognition.face_encodings(img_test)[0]
+        results = face_recognition.compare_faces([encodeElon], encodeElonTest)
+        response = {"result": bool(results[0])}
+        return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
