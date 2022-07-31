3ed624c1fa9a54859e88ce0e44da6e4a2c6e4df6
alishawadhwaniya <aw377541@gmail.com>
2022-07-31 21:43:05 +0530

    Helo

--- app/wsgi.py
+++ app/wsgi.py
@@ -2,4 +2,6 @@ from app.main import app
 import os
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT')))
+    port = int(os.environ.get('PORT', 5000))
+    print(port)
+    app.run(host='0.0.0.0', port=port)
