a365bb5b2f8b7dcf9a3a88beaca673261b713641
alishawadhwaniya <aw377541@gmail.com>
2022-07-31 21:59:44 +0530

    Welcome

--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,7 @@
-from app.main import app
-import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    print(port)
-    app.run(host='0.0.0.0', port=port)
+# from app.main import app
+# import os
+#
+# if __name__ == "__main__":
+#     port = int(os.environ.get('PORT', 5000))
+#     print(port)
+#     app.run(host='0.0.0.0', port=port)
