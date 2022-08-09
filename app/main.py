8bc4f863b2442f584756c884acd04d041e3f5b82
alishawadhwaniya <aw377541@gmail.com>
2022-08-09 16:34:53 +0530

    Added GET Method in Authenticate

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
+
 @app.route('/')
 def welcome():
     return 'Server is Running. The endpoint to verify face is /authenticate and pass links for both images as image and testImage in params'
 
 
-@app.route('/authenticate', methods=['POST'])
+@app.route('/authenticate', methods=['GET','POST'])
 def check_face():
     with app.app_context():
         image1 = urllib.request.urlopen(request.args.get('image'))
