95b9e067ec9c740ec8a35f8b04b74b6177749a86
alishawadhwaniya <aw377541@gmail.com>
2022-08-14 11:50:38 +0530

    Final Changes

--- app/main.py
+++ app/main.py
@@ -15,15 +15,23 @@ def welcome():
     return 'Server is Running. The endpoint to verify face is /authenticate and pass links for both images as image and testImage in params'
 
 
-@app.route('/authenticate', methods=['GET','POST'])
+@app.route('/authenticate', methods=['POST'])
 def check_face():
     with app.app_context():
-        image1 = urllib.request.urlopen(request.args.get('image'))
-        image2 = urllib.request.urlopen(request.args.get('testImage'))
+        print("Check Face Called")
+        image1 = urllib.request.urlopen(request.json['image'])
+        image2 = urllib.request.urlopen(request.json['testImage'])
         img = cv2.cvtColor(face_recognition.load_image_file(image1), cv2.COLOR_BGR2RGB)
         img_test = cv2.cvtColor(face_recognition.load_image_file(image2), cv2.COLOR_BGR2RGB)
-        encodeElon = face_recognition.face_encodings(img)[0]
-        encodeElonTest = face_recognition.face_encodings(img_test)[0]
+        try:
+            encodeElon = face_recognition.face_encodings(img)[0]
+        except IndexError as e:
+            return "Unable to Detect Face in Image 1"
+        try:
+            encodeElonTest = face_recognition.face_encodings(img_test)[0]
+        except IndexError as e:
+            return "Unable to Detect Face in Image 2"
         results = face_recognition.compare_faces([encodeElon], encodeElonTest)
         response = {"result": bool(results[0])}
+        print(response)
         return jsonify(response)
